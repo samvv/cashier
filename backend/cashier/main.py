7ac21eb3a03c1845cc0fe0d7f39da3e40523deb5
Sam Vervaeck <samvv@pm.me>
2025-12-06 19:27:10 +0100

    Configure the lifespan for FastAPI

--- backend/cashier/main.py
+++ backend/cashier/main.py
@@ -1,13 +1,23 @@
-# SQLModel.metadata.create_all needs to have the SQLModels loaded in before creating them
-import cashier.models
-
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from sqlmodel import SQLModel
 
-from cashier.core.db.config import engine
+# SQLModel.metadata.create_all needs to have the SQLModels loaded in before creating them
+from cashier.constants import DB_URI
+from cashier.core import build_engine
+import cashier.models
 import cashier.api as api
 
-SQLModel.metadata.create_all(bind=engine)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    engine = build_engine(DB_URI)
+    SQLModel.metadata.create_all(engine)
+    app.state.engine = engine
+    yield
+    engine.dispose()
 
-app = FastAPI()
+
+app = FastAPI(lifespan=lifespan)
+
+
 app.include_router(api.product_router)

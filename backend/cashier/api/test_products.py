07fed414a687f479cfd30b1a002e90f5c7f2a073
Sam Vervaeck <samvv@pm.me>
2025-12-06 19:23:12 +0100

    Extend the test in test_products.py a bit

--- backend/cashier/api/test_products.py
+++ backend/cashier/api/test_products.py
@@ -2,5 +2,8 @@ from fastapi.testclient import TestClient
 
 def test_all_products(client: TestClient):
     products = client.get("/products")
-    assert products.status_code == 200
-    assert len(products.json()) > 0
+    assert(products.status_code == 200)
+    data = products.json()
+    assert(len(data) == 1)
+    product_1 = data[0]
+    assert(product_1['ean'] == '2052552')
